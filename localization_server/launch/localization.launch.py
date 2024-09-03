b8866a13bf3db8437312ad655bc81094bd14c2c9
Ivo Georgiev <ivogeorg@gmail.com>
2024-09-03 21:51:17 +0000

    localization_server revised
    Added an argument for the AMCL config file. This is too many arguments.
    Have to figure out how to select config files based on the value of the
    use_sim_time argument value. For path_planner_server that will be
    crucial since there are too many config files to create separate
    arguments for.

--- localization_server/launch/localization.launch.py
+++ localization_server/launch/localization.launch.py
@@ -11,15 +11,15 @@ from launch_ros.actions import Node
 
 
 def generate_launch_description():
-    # SIMULATOR <======OR=====> REAL ROBOT LAB
-    use_sim_time = False
-    
     pkg_name = 'localization_server'
+
+    use_sim_time_f = LaunchConfiguration('use_sim_time')
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value=TextSubstitution(text='True'))
+    
     pkg_share_name = FindPackageShare(pkg_name)
-    config_dir_name = 'config'
 
     # Rviz2 config file path argument
-    rviz_config_file_name = 'loconfig.rviz'
+    rviz_config_file_name = 'localization_config.rviz'
     rviz_config_dir_name = 'rviz'
     rviz_config_dir = PathJoinSubstitution([pkg_share_name, rviz_config_dir_name, rviz_config_file_name])
     rviz_config_dir_arg = DeclareLaunchArgument('d', default_value=rviz_config_dir)
@@ -31,13 +31,13 @@ def generate_launch_description():
             executable='rviz2',
             output='screen',
             name='rviz_node',
-            parameters=[{'use_sim_time': use_sim_time}],
+            parameters=[{'use_sim_time': use_sim_time_f}],
             arguments=['-d', rviz_config_dir_f])
 
 
     # map serve node
     map_svr_pkg_name = 'map_server'
-    map_config_dir_name = 'config'
+    map_dir_name = 'maps'
     default_map_file_name = 'warehouse_map_sim.yaml'
 
     # Capture command line or launch file argument 'map_file'
@@ -46,24 +46,32 @@ def generate_launch_description():
     # This is only the file name and not the full path, so
     # construct the path and then pass to the 'yaml_filename'
     # parameter of the 'map_server' executable.
+    map_file_f = LaunchConfiguration('map_file')
     map_file_arg = DeclareLaunchArgument(
                         'map_file', 
                         default_value=TextSubstitution(text=default_map_file_name))
-    map_file_f = LaunchConfiguration('map_file')
-    map_file_path = PathJoinSubstitution([FindPackageShare(map_svr_pkg_name), map_config_dir_name, map_file_f])
+    map_file_path = PathJoinSubstitution([FindPackageShare(map_svr_pkg_name), map_dir_name, map_file_f])
 
     map_server_node = Node(
             package='nav2_map_server',
             executable='map_server',
             name='map_server',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time}, 
+            parameters=[{'use_sim_time': use_sim_time_f}, 
                         {'yaml_filename':map_file_path}])
 
+
+    amcl_config_file_f = LaunchConfiguration('amcl_config')
+    amcl_config_file_arg = DeclareLaunchArgument('amcl_config', default_value=TextSubstitution(text='amcl_config_sim.yaml'))
+
     # localization node (amcl)
-    amcl_config_file_name = 'amcl_config.yaml'
+    # if use_sim_time_f: # would this work
+    #     amcl_config_file_name = 'amcl_config_sim.yaml'
+    # else:
+    #     amcl_config_file_name = 'amcl_config_lab.yaml'
     amcl_config_dir_name = 'config'
-    amcl_config_file_path = PathJoinSubstitution([pkg_share_name, amcl_config_dir_name, amcl_config_file_name])
+    # amcl_config_file_path = PathJoinSubstitution([pkg_share_name, amcl_config_dir_name, amcl_config_file_name])
+    amcl_config_file_path = PathJoinSubstitution([pkg_share_name, amcl_config_dir_name, amcl_config_file_f])
     amcl_node = Node(
             package='nav2_amcl',
             executable='amcl',
@@ -77,16 +85,20 @@ def generate_launch_description():
             executable='lifecycle_manager',
             name='lifecycle_manager_localization',
             output='screen',
-            parameters=[{'use_sim_time': use_sim_time},
+            parameters=[{'use_sim_time': use_sim_time_f},
                         {'autostart': True},
                         {'node_names': ['map_server', 'amcl']}]
     )
     
     return LaunchDescription([
+        use_sim_time_arg,
         rviz_config_dir_arg,
         rviz_node,
+        amcl_config_file_arg,
         map_file_arg,
         LogInfo(msg=["Map file path: ", map_file_path]),
+        LogInfo(msg=["use_sim_time: ", use_sim_time_f]),
+        LogInfo(msg=["AMCL config file: ", amcl_config_file_f]),
         map_server_node,
         amcl_node,
         lifecycle_manager_node
