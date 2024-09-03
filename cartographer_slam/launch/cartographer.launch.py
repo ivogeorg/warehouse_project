cd37276e466dc6378e4c8df492cf5f2878ee117d
Ivo Georgiev <ivogeorg@gmail.com>
2024-09-03 20:49:28 +0000

    cartographer_slam revised

--- cartographer_slam/launch/cartographer.launch.py
+++ cartographer_slam/launch/cartographer.launch.py
@@ -8,22 +8,17 @@ from launch_ros.substitutions import FindPackageShare
 from launch_ros.actions import Node
 
 def generate_launch_description():
+    use_sim_time_f = LaunchConfiguration('use_sim_time')
+    use_sim_time_arg = DeclareLaunchArgument('use_sim_time', default_value=TextSubstitution(text='True'))
+
     pkg_name = 'cartographer_slam'
     pkg_share_path = FindPackageShare(pkg_name)
     config_dir_name = 'config'
 
-    cartographer_config_file_name = 'cartographer_lab.lua'
-
-    # environment_arg = DeclareLaunchArgument('environ', default_value='lab')
-    # environment_f = LaunchConfiguration('environ')
-    # if (environment_f == 'lab'):
-    #     cartographer_config_file_name = 'cartographer_lab.lua'
-    # else:
-    #     cartographer_config_file_name = 'cartographer_sim.lua'
-
+    cartographer_config_file_name = 'cartographer_sim.lua'
 
     # Rviz2 config file path argument
-    rviz_config_file_name = 'caconfig.rviz'
+    rviz_config_file_name = 'cartographer_config.rviz'
     rviz_config_dir_name = 'rviz'
     rviz_config_dir = PathJoinSubstitution([pkg_share_path, rviz_config_dir_name, rviz_config_file_name])
     rviz_config_dir_arg = DeclareLaunchArgument('d', default_value=rviz_config_dir)
@@ -35,49 +30,49 @@ def generate_launch_description():
             executable='rviz2',
             output='screen',
             name='rviz_node',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time_f}],
             arguments=['-d', rviz_config_dir_f])
 
     # cartographer node (SLAM)
-    config_dir_arg = DeclareLaunchArgument("configuration_directory", default_value=PathJoinSubstitution([pkg_share_path, config_dir_name]))
-    config_basename_arg = DeclareLaunchArgument("configuration_basename", default_value=TextSubstitution(text=cartographer_config_file_name))
-
     cartographer_config_dir = LaunchConfiguration("configuration_directory")
+    config_dir_arg = DeclareLaunchArgument("configuration_directory", default_value=PathJoinSubstitution([pkg_share_path, config_dir_name]))
+
     configuration_basename = LaunchConfiguration("configuration_basename")
+    config_basename_arg = DeclareLaunchArgument("configuration_basename", default_value=TextSubstitution(text=cartographer_config_file_name))
 
     cartographer_node = Node(
             package='cartographer_ros', 
             executable='cartographer_node', 
             name='cartographer_node',
             output='screen',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time_f}],
             arguments=['-configuration_directory', cartographer_config_dir,
                        '-configuration_basename', configuration_basename]
     )
 
     # occupancy grid node
-    resolution_arg = DeclareLaunchArgument('resolution', default_value='0.05')
-    publish_period_arg = DeclareLaunchArgument('publish_period_sec', default_value='1.0')
-
     resolution_f = LaunchConfiguration('resolution')
+    resolution_arg = DeclareLaunchArgument('resolution', default_value='0.05')
+
     publish_period_f = LaunchConfiguration('publish_period_sec')
+    publish_period_arg = DeclareLaunchArgument('publish_period_sec', default_value='1.0')
 
     ocupancy_grid_node = Node(
             package='cartographer_ros',
             executable='cartographer_occupancy_grid_node', # from Discord
             output='screen',
             name='occupancy_grid_node',
-            parameters=[{'use_sim_time': True}],
+            parameters=[{'use_sim_time': use_sim_time_f}],
             arguments=['-resolution', resolution_f, 
                        '-publish_period_sec', publish_period_f]    
     )
     
     return LaunchDescription([
-        # environment_arg,
+        use_sim_time_arg,
         rviz_config_dir_arg,
         rviz_node,
         LogInfo(msg=["Rviz2 config file path: ", rviz_config_dir_f]),
-        # LogInfo(msg=["Environment: ", environment_f]),
+        LogInfo(msg=["use_sim_time: ", use_sim_time_f]),
         LogInfo(msg=["Lua file: ", cartographer_config_file_name]),
         config_dir_arg,
         config_basename_arg,
